9e7b9ed0f9f221ed88d88a3e88479ce4529334db
jayson7 <lexxiijoo70@gmail.com>
2024-10-06 19:05:47 +0100

    basic config of pages

---
+++ frontend/src/components/Categories/categories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Categories() {
+  return (
+    <div>
+      <div className="container">
+        <div className="d-grid">
+          <h1 className="display-1">All Products</h1>
+          <small>--Categories</small>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Categories;
--- frontend/src/components/Navigator/navbar.jsx
+++ frontend/src/components/Navigator/navbar.jsx
@@ -1,22 +1,103 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import NotFound from "./components/NotFound";
-import { Container } from "@mui/material";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
-const App = () => {
+// pages
+import Homepage from "../Home/homepage.jsx";
+import Categories from "../Categories/categories";
+import NotFound from "../Page4040/NotFound.jsx";
+
+const Navbar = () => {
   return (
-    <Router>
-      <Container>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route component={NotFound} />
-        </Switch>
-      </Container>
-    </Router>
+    <div>
+      <Router>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">
+              Navbar
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link active" aria-current="page" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/categories">
+                    All Products
+                  </Link>
+                </li>
+                <li className="nav-item dropdown">
+                  <Link
+                    className="nav-link dropdown-toggle"
+                    to="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown
+                  </Link>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <li>
+                      <Link className="dropdown-item" to="#">
+                        Action
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="#">
+                        Another Action
+                      </Link>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="#">
+                        Something Else Here
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link disabled"
+                    to="#"
+                    aria-disabled="true"
+                  >
+                    Disabled
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
-export default App;
+export default Navbar;
