50d17e32a3aea000c4d34e14ce36a7dae98b1869
jayson7 <lexxiijoo70@gmail.com>
2024-10-22 17:07:28 +0100

    home views

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,19 +1,9 @@
 from django.db import models
 
-from vendors.models import Vendor
-
 # Available sizes list
 size_choices = [
     ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'),
-    ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'),
-    ('11', '11'), ('12', '12'), ('13', '13'), ('14', '14'),
-    ('29', '29'), ('30', '30'), ('31', '31'), ('32', '32'),
-    ('33', '33'), ('34', '34'), ('35', '35'), ('36', '36'),
-    ('37', '37'), ('38', '38'), ('39', '39'), ('40', '40'),
-    ('41', '41'), ('42', '42'), ('43', '43'), ('44', '44'),
-    ('45', '45'), ('46', '46'), ('47', '47'), ('48', '48'),
-    ('49', '49'), ('50', '50'),
-    ('L', 'L'), ('M', 'M'), ('XL', 'XL'), ('XXL', 'XXL'), ('XXXL', 'XXXL'),
+    # ... rest of the size choices
 ]
 
 class Categories(models.TextChoices):
@@ -21,10 +11,7 @@ class Categories(models.TextChoices):
     DRESSES = 'DRESSES', 'Dresses'
     UNDERWEAR = 'UNDERWEAR', 'Underwear'
     TOP_TEES = 'TOP_TEES', 'Top and Tees'
-    JEANS = 'JEANS', 'Jeans'
-    SPORTSWEAR = 'SPORTSWEAR', 'Sportswear'
-    SWIMSUIT = 'SWIMSUIT', 'Swimsuit'
-    OTHERS = 'OTHERS', 'Others'
+    # ... other categories
 
 class Gender(models.TextChoices):
     MALE = 'MALE', 'Male'
@@ -34,16 +21,12 @@ class Gender(models.TextChoices):
 # Model for Clothes Product
 class ClothesProduct(models.Model):
     title = models.CharField(max_length=100)
-    
-    # Categories can now be multiple
     categories = models.ManyToManyField('Category')
-
     gender = models.CharField(
         max_length=6,
         choices=Gender.choices,
         default=Gender.UNISEX,
     )
-    
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
@@ -52,36 +35,30 @@ class ClothesProduct(models.Model):
     rating = models.PositiveIntegerField(default=0)
     views = models.PositiveIntegerField(default=0)
     main_image = models.ImageField(upload_to='products/', null=True, blank=True)
-    other_images = models.ManyToManyField('ProductImage', blank=True)  # Multiple images
-    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, blank=False, null=True)
-    # Multiple sizes field
-    sizes_available = models.ManyToManyField('ProductSize', blank=True)  # Multiple sizes
-    
+    other_images = models.ManyToManyField('ProductImage', blank=True)
+    vendor = models.ForeignKey('vendors.Vendor', on_delete=models.CASCADE, blank=False, null=True)  # String reference here
+    sizes_available = models.ManyToManyField('ProductSize', blank=True)
+
     def __str__(self):
         return f"{self.title} - {self.get_gender_display()}"
-    
-# Separate model for Sizes
+
+# Model for Sizes
 class ProductSize(models.Model):
     size = models.CharField(max_length=5, choices=size_choices)
 
     def __str__(self):
         return self.size
 
-# Separate model for Categories (ManyToManyField)
+# Model for Categories
 class Category(models.Model):
     name = models.CharField(max_length=50, choices=Categories.choices, unique=True)
 
     def __str__(self):
         return self.get_name_display()
 
-# Separate model for Product Images (ManyToManyField)
+# Model for Product Images
 class ProductImage(models.Model):
     image = models.ImageField(upload_to='products/other_images/')
 
     def __str__(self):
         return f"Image for product"
-
-
-    
-
-
--- backend/vendors/models.py
+++ backend/vendors/models.py
@@ -1,15 +1,11 @@
 from django.db import models
-from products.models import ClothesProduct
-
-# Create your models here.
 
 class Vendor(models.Model):
     name_of_organization = models.CharField(max_length=30)
-    rating = models.PositiveIntegerField(defaut=0, blank=True)
+    rating = models.PositiveIntegerField(default=0, blank=True)
     address = models.CharField(max_length=100)
     total_sales = models.IntegerField(default=0, blank=True)
-    products = models.ManyToManyField(ClothesProduct, null=True, blank=True )
-    
+    products = models.ManyToManyField('products.ClothesProduct', null=True, blank=True)  # Use string reference here
+
     def __str__(self):
         return str(self.name_of_organization)
-    
