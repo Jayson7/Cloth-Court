17b3c1f7ffd1f531dc7b7028652b092073f32479
jayson7 <lexxiijoo70@gmail.com>
2024-11-08 18:55:00 +0100

    start error

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -1,30 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
-# Create your models here.
 
 class User(AbstractUser):
     username = models.CharField(max_length=100)
-    email = models.EmailField(unique = True)
+    email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',  # Avoids clash with the default Group model
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set',  # Avoids clash with the default Permission model
+        blank=True
+    )
+
+    @property
     def profile(self):
-        profile = Profile.objects.get(user=self)
+        return Profile.objects.get(user=self)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=100)
     bio = models.CharField(max_length=1000)
-    verified = models.BooleanField(default = False)
+    verified = models.BooleanField(default=False)
 
+# Signal to create and save Profile automatically
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
 post_save.connect(create_user_profile, sender=User)
-post_save.connect(save_user_profile, sender=User)
--- backend/user_profile/serializer.py
+++ backend/user_profile/serializer.py
@@ -17,7 +17,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
     full_name = serializers.CharField(write_only=True, required=True)
-
+    
     class Meta:
         model = User
         fields = ['full_name', 'email', 'username', 'password', 'password2']
