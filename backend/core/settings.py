e99f278f01e9d465232137ce10592e6a330fe343
jayson7 <lexxiijoo70@gmail.com>
2024-11-13 12:00:49 +0100

    configuration of authentication

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -146,7 +146,8 @@ REST_FRAMEWORK = {
 
 # cors headers config 
 
-CORS_ALLOWED_ORIGINS = ['https://localhost:3000', "http://localhost:19000",]
+# CORS_ALLOWED_ORIGINS = ['https://localhost:3000', "http://localhost:19000",]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_METHODS = (
     "DELETE",
--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 
+
 class User(AbstractUser):
     username = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
