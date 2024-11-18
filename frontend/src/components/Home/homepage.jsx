8c3a04f7a0df9bcb44f4f2df0f4c42b155b19721
jayson7 <lexxiijoo70@gmail.com>
2024-11-18 16:49:58 +0100

    editing

--- frontend/src/components/Home/homepage.jsx
+++ frontend/src/components/Home/homepage.jsx
@@ -1,29 +1,36 @@
 import React from "react";
 import "./home.css";
-import hero from "../../images/hero-bg.jpg";
+import hero from "../../images/hero-bg.png";
+// extensions
+
+import LatestProduct from "./latest product/latest";
+
 function homepage() {
   return (
-    <div className="homeWrapper">
-      <div className="container d-flex justify-content-around align-items-center flex-wrap">
-        <div className="row">
-          <div className="tittle-box1 col-md-6 col-sm-12">
-            <div className="title-text">Raining Offers For Hot Summer!</div>
-            <div className="tittle-text-mini">25% Off On All Products</div>
-            <div className="tittle-button">
-              <button className="btn btn-transparent py-3 px-4">
+    <div>
+      <div className="homeWrapper">
+        <div className="hero-container ">
+          <div className="tittle-box1">
+            <h1 className="title-text">
+              Raining Offers For <br /> Hot Summer!
+            </h1>
+            <h4 className="title-text-mini">25% Off On All Products</h4>
+            <div className="title-button mt-5">
+              <button className="btn btn-transparent text-white py-2 px-4 ">
                 Shop Now
               </button>
-              <button className="btn btn-success">Find More</button>
+              <button className="btn btn-success py-2 px-4 mx-4">
+                Find More
+              </button>
             </div>
           </div>
-        </div>
 
-        <div className="tittle-box2 col-md-6 col-sm-12">
-          <div className="title-box-mini">
+          <div className="title-box2 ">
             <img src={hero} alt="" />
           </div>
         </div>
       </div>
+      <LatestProduct />
     </div>
   );
 }
--- frontend/src/components/Navigator/navbar.jsx
+++ frontend/src/components/Navigator/navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   return (
     <div>
       <Router>
-        <nav className="navbar navbar-expand-lg  navbar-dark bg-light">
+        <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-light">
           <div className="container-fluid ">
             <Link className="navbar-brand" to="/">
               <img
