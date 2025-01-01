d17a05d50bde96657c90c69771e26dc9956f3778
jayson7 <lexxiijoo70@gmail.com>
2025-01-01 20:04:23 +0100

    more features & auth

--- backend/user_profile/serializer.py
+++ backend/user_profile/serializer.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
