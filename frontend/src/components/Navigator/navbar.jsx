aa62596a9ae239d7cddd56bf346ecde9179e1e50
jayson7 <lexxiijoo70@gmail.com>
2025-01-28 18:41:37 +0100

    authentication

--- frontend/src/components/Navigator/navbar.jsx
+++ frontend/src/components/Navigator/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
 import Dropdown from "react-bootstrap/Dropdown";
 import "./nav.css";
 import Homepage from "../Home/homepage.jsx";
@@ -16,11 +16,11 @@ import logo from "../../images/logo_dark.png";
 import { FaCartPlus } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import TokenService from "../../tokenService.js";
-import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const navigate = useNavigate();
+
   // Check token validity on component mount
   useEffect(() => {
     const tokenValid = TokenService.getAccessTokenValidity();
@@ -29,7 +29,6 @@ const Navbar = () => {
   }, []);
 
   // Handle Logout
-
   const handleLogout = () => {
     TokenService.clearToken(); // Clear storage
     setIsAuthenticated(false); // Update authentication state
@@ -38,138 +37,133 @@ const Navbar = () => {
 
   return (
     <div>
-      <Router>
-        <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-light">
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">
-              <img
-                src={logo}
-                width="40"
-                className="rounded mx-2 mb-2"
-                height="50"
-                alt=""
-              />
-              <span className="logo-text">
-                CloTh<span>Court</span>
-              </span>
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav mx-auto mb-4 mb-lg-2 mx-4">
-                <li className="nav-item">
-                  <Link className="nav-link" aria-current="page" to="/">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/products">
-                    All Products
-                  </Link>
-                </li>
-                <li>
-                  <Link className="nav-link" to="/categories">
-                    Categories
-                  </Link>
-                </li>
-                <li>
-                  <Link className="nav-link" to="/products">
-                    Best Sellers
-                  </Link>
-                </li>
-                <li>
-                  <Link className="nav-link" to="/products">
-                    Track Order
-                  </Link>
-                </li>
-                <li>
-                  <Link className="nav-link" to="/products">
-                    Chat with us
-                  </Link>
-                </li>
-              </ul>
-              <div className="nav-logos action-group d-flex flex-wrap align-content-center justify-content-around">
-                <div className="cart">
-                  <span>0</span>
-                  <FaCartPlus />
-                </div>
-                <div className="balance mt-2">₦ 0.00</div>
-
-                <Dropdown>
-                  <Dropdown.Toggle id="dropdown-basic">
-                    <FaUser />
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu>
-                    {isAuthenticated ? (
-                      <>
-                        <Dropdown.Item className="text-center py-2 px-4">
-                          <Link
-                            to="/profile"
-                            className="text-decoration-none text-dark text-center"
-                          >
-                            Profile
-                          </Link>
-                        </Dropdown.Item>
-                        <Dropdown.Item
-                          className="text-center py-2 px-4"
-                          onClick={handleLogout}
-                        >
-                          Logout
-                        </Dropdown.Item>
-                      </>
-                    ) : (
-                      <>
-                        <Dropdown.Item className="text-center py-2 px-4">
-                          <Link
-                            to="/login"
-                            className="text-decoration-none text-dark text-center"
-                          >
-                            Login
-                          </Link>
-                        </Dropdown.Item>
-                        <Dropdown.Item className="text-center py-2 px-4">
-                          <Link
-                            to="/register"
-                            className="text-decoration-none text-dark text-center"
-                          >
-                            Sign Up
-                          </Link>
-                        </Dropdown.Item>
-                      </>
-                    )}
-                  </Dropdown.Menu>
-                </Dropdown>
+      <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <img
+              src={logo}
+              width="40"
+              className="rounded mx-2 mb-2"
+              height="50"
+              alt=""
+            />
+            <span className="logo-text">
+              CloTh<span>Court</span>
+            </span>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-4 mb-lg-2 mx-4">
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/products">
+                  All Products
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/categories">
+                  Categories
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/products">
+                  Best Sellers
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/products">
+                  Track Order
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/products">
+                  Chat with us
+                </Link>
+              </li>
+            </ul>
+            <div className="nav-logos action-group d-flex flex-wrap align-content-center justify-content-around">
+              <div className="cart">
+                <span>0</span>
+                <FaCartPlus />
               </div>
+              <div className="balance mt-2">₦ 0.00</div>
+
+              <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic">
+                  <FaUser />
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  {isAuthenticated ? (
+                    <>
+                      <Dropdown.Item className="text-center py-2 px-4">
+                        <Link
+                          to="/profile"
+                          className="text-decoration-none text-dark text-center"
+                        >
+                          Profile
+                        </Link>
+                      </Dropdown.Item>
+                      <Dropdown.Item
+                        className="text-center py-2 px-4"
+                        onClick={handleLogout}
+                      >
+                        Logout
+                      </Dropdown.Item>
+                    </>
+                  ) : (
+                    <>
+                      <Dropdown.Item className="text-center py-2 px-4">
+                        <Link
+                          to="/login"
+                          className="text-decoration-none text-dark text-center"
+                        >
+                          Login
+                        </Link>
+                      </Dropdown.Item>
+                      <Dropdown.Item className="text-center py-2 px-4">
+                        <Link
+                          to="/register"
+                          className="text-decoration-none text-dark text-center"
+                        >
+                          Sign Up
+                        </Link>
+                      </Dropdown.Item>
+                    </>
+                  )}
+                </Dropdown.Menu>
+              </Dropdown>
             </div>
           </div>
-        </nav>
+        </div>
+      </nav>
 
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/product/:id/" element={<ProductDetail />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/profile" element={<UserProfile />} />
-          <Route path="/deposit" element={<Deposit />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/product/:id/" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/deposit" element={<Deposit />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import store from "./components/store";
 
 import Navbar from "./components/Navigator/navbar";
+
 // As of React 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <Navbar />
+    <BrowserRouter>
+      {" "}
+      {/* Wrap your application in BrowserRouter */}
+      <Navbar />
+    </BrowserRouter>
   </Provider>
 );
