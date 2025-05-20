441e9b55be2fccfb1c56a5226e6b1e93706a56f8
jayson7 <lexxiijoo70@gmail.com>
2025-05-20 08:47:15 +0100

    cart, payment isues

--- backend/apps/models.py
+++ backend/apps/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
-# Create your models here.
+from user_profile.models import User
+from shipping.models import ShippingAddress
+class Order(models.Model):
+    product = models.CharField(max_length=200)
+    quantiy= models.IntegerField(default=1)
+    client = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    paid = models.BooleanField(default=False)
+    shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.CASCADE, blank=False)
+    vendor = models.ForeignKey()
--- backend/payments/models.py
+++ backend/payments/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from user_profile.models import User 
+from vendors.models import Vendor
 
 # Create your models here.
 
 class PreviousPaymemts(models.Model):
-    pass 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    date_of_payment = models.DateTimeField(auto_now_add=True)
+    paid = models.BooleanField(default=False)
+    vendor = models.ForeignKey()
+    
+    
 
 class PaymentStatus(models.Model):
     pass 
